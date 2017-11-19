45744bf01b7eaa9539f827f9cd7a68d0ec32adb9
Jin Xu <s1673820@sms.ed.ac.uk>
2017-11-19 21:07:55 +0000

    add_files

--- forward_model.py
+++ forward_model.py
@@ -258,10 +258,10 @@ with tf.Session() as sess:
     ckpt_file = args.save_dir + '/params_' + args.data_set + '.ckpt'
     print('restoring parameters from', ckpt_file)
     saver.restore(sess, ckpt_file)
-
+    new_vars = []
     ll = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
     for item in ll:
-        item.name = item.name.replace("model", "forward")
+        new_vars.append(tf.Variable(item, name=name.replace('model', 'forward')))
     ll = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
     print([item.name for item in ll])
 
