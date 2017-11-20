982ab01b8c3c39e75858b9a9255c651fc31e1d84
Jin Xu <s1673820@sms.ed.ac.uk>
2017-11-20 11:43:09 +0000

    fix

--- backward_model.py
+++ backward_model.py
@@ -90,8 +90,8 @@ if args.data_set == 'imagenet' and args.class_conditional:
 DataLoader = {'cifar': cifar10_data.DataLoader,
               'imagenet': imagenet_data.DataLoader,
               'celeba': celeba_data.DataLoader}[args.data_set]
-train_data = DataLoader(args.data_dir, 'train', args.batch_size * args.nr_gpu,
-                        rng=rng, shuffle=True, return_labels=args.class_conditional)
+#train_data = DataLoader(args.data_dir, 'train', args.batch_size * args.nr_gpu,
+#                        rng=rng, shuffle=True, return_labels=args.class_conditional)
 test_data = DataLoader(args.data_dir, 'valid', args.batch_size *
                        args.nr_gpu, shuffle=False, return_labels=args.class_conditional)
 obs_shape = test_data.get_observation_size()  # e.g. a tuple (32,32,3)
@@ -256,12 +256,6 @@ lr = args.learning_rate
 
 with tf.Session() as sess:
 
-    feed_dict = make_feed_dict(
-        train_data.next(args.init_batch_size), init=True)
-    train_data.reset()  # rewind the iterator back to 0 to do one full epoch
-    sess.run(initializer, feed_dict)
-    print('initializing the model...')
-
     ckpt_file = args.save_dir + '/params_' + args.data_set + '.ckpt'
     print('restoring parameters from', ckpt_file)
     saver.restore(sess, ckpt_file)
@@ -279,6 +273,7 @@ with tf.Session() as sess:
     var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="model_1")
     print(len(var_list))
     saver1 = tf.train.Saver(var_list=var_list)
+    sess.run(tf.global_variables_initializer())
     saver1.save(sess, "/data/ziz/jxu/save-test" + '/params_' +
                args.data_set + '.ckpt')
     quit()
