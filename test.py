84796f6c644b3909565801eddff1436c26fb8661
Jin Xu <s1673820@sms.ed.ac.uk>
2017-11-20 15:49:12 +0000

    fix

--- test.py
+++ test.py
@@ -7,14 +7,10 @@ import backward_model as bm
 with tf.Session() as sess:
     vs = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='model/')
     vs1 = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='model_1/')
-    print(len(vs))
-    print(len(vs1))
-    print([item.name for item in vs])
-    print([item.name for item in vs1])
-    quit()
 
-    saver = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='model'))
-    saver1 = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='model_1'))
+
+    saver = tf.train.Saver(var_list=vs)
+    saver1 = tf.train.Saver(var_list=vs1)
 
 
     ckpt_file = "/data/ziz/jxu/save-forward" + '/params_' + "celeba" + '.ckpt'
