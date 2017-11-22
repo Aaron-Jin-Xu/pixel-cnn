820103c51abb7f026c12e215cbdb4791ad116b43
Jin Xu <s1673820@sms.ed.ac.uk>
2017-11-22 21:40:37 +0000

    fix

--- prior.py
+++ prior.py
@@ -2,7 +2,7 @@ import numpy as np
 from PIL import Image
 from data.celeba_data import read_imgs
 
-imgs = read_imgs("/data/ziz/not-backed-up/jxu/CelebA/celeba32-valid")
+imgs = read_imgs("/data/ziz/not-backed-up/jxu/CelebA/celeba32-train")
 imgs = np.array([imgs==i for i in range(256)], dtype=np.uint32)
 imgs = imgs.sum(axis=1)
 np.savez_compressed("prior", arr=imgs)
