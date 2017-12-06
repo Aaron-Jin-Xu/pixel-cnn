2e47dd08656a97ed8b086afbaa519e0593d7d19d
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-06 16:58:50 +0000

    fix

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ with tf.Session() as sess:
         #print(target_pixels[0])
         if target_pixels[0][0] is None:
             break
-        #pr = get_prior(prior, target_pixels)
+        pr = get_prior(prior, target_pixels)
         backward_ms = ms.copy()
         for idx in range(len(target_pixels)):
             p = target_pixels[idx]
@@ -94,14 +94,13 @@ with tf.Session() as sess:
 
         pars1 = params_to_dis(o1, fm.args.nr_logistic_mix)
         pars2 = params_to_dis(o2, fm.args.nr_logistic_mix)
-        pars = pars1 * pars2 #/ pr[:, 0, :]
+        pars = pars1 * pars2 / pr[:, 0, :]
         #pars[:, 0] = 0.
         #pars[:, 255] = 0.
         #pars = np.power(pars, 0.5)
         pars = pars.astype(np.float64)
-        print(pars)
         pars = pars / np.sum(pars, axis=-1)[:, None]
-        #rgb_record.append(np.array([pars1, pars2, pars, pars/pr[:, 0, :]]))
+        rgb_record.append(np.array([pars1, pars2, pars, pars/pr[:, 0, :]]))
         color_r = []
         for i in range(pars.shape[0]):
             color_r.append(np.argmax(np.random.multinomial(1, pars[i, :])))
@@ -109,13 +108,13 @@ with tf.Session() as sess:
 
         pars1 = params_to_dis(o1, fm.args.nr_logistic_mix, r=color_r)
         pars2 = params_to_dis(o2, fm.args.nr_logistic_mix, r=color_r)
-        pars = pars1 * pars2 #/ pr[:, 1, :]
+        pars = pars1 * pars2 / pr[:, 1, :]
         #pars[:, 0] = 0.
         #pars[:, 255] = 0.
         #pars = np.power(pars, 0.5)
         pars = pars.astype(np.float64)
         pars = pars / np.sum(pars, axis=-1)[:, None]
-        #rgb_record.append(np.array([pars1, pars2, pars, pars/pr[:, 1, :]]))
+        rgb_record.append(np.array([pars1, pars2, pars, pars/pr[:, 1, :]]))
         color_g = []
         for i in range(pars.shape[0]):
             color_g.append(np.argmax(np.random.multinomial(1, pars[i, :])))
@@ -123,13 +122,13 @@ with tf.Session() as sess:
 
         pars1 = params_to_dis(o1, fm.args.nr_logistic_mix, r=color_r, g=color_g)
         pars2 = params_to_dis(o2, fm.args.nr_logistic_mix, r=color_r, g=color_g)
-        pars = pars1 * pars2 #/ pr[:, 2, :]
+        pars = pars1 * pars2 / pr[:, 2, :]
         #pars[:, 0] = 0.
         #pars[:, 255] = 0.
         #pars = np.power(pars, 0.5)
         pars = pars.astype(np.float64)
         pars = pars / np.sum(pars, axis=-1)[:, None]
-        #rgb_record.append(np.array([pars1, pars2, pars, pars/pr[:, 2, :]]))
+        rgb_record.append(np.array([pars1, pars2, pars, pars/pr[:, 2, :]]))
         color_b = []
         for i in range(pars.shape[0]):
             color_b.append(np.argmax(np.random.multinomial(1, pars[i, :])))
@@ -137,7 +136,7 @@ with tf.Session() as sess:
 
         color = np.array([color_r, color_g, color_b]).T
         #print(color)
-        #dis_record.append(np.array(rgb_record))
+        dis_record.append(np.array(rgb_record))
 
         for idx in range(len(target_pixels)):
             p = target_pixels[idx]
@@ -147,9 +146,9 @@ with tf.Session() as sess:
 
         data_record.append(d.copy())
 
-    #dis_record = np.array(dis_record)
+    dis_record = np.array(dis_record)
     data_record = np.array(data_record)
-    #np.savez_compressed("/data/ziz/jxu/inpainting_dis", dis=dis_record)
-    np.savez_compressed("/data/ziz/jxu/inpaintng_img", img=data_record)
+    np.savez_compressed("/data/ziz/jxu/inpainting_dis", dis=dis_record)
+    np.savez_compressed("/data/ziz/jxu/inpainting_img", img=data_record)
     img = Image.fromarray(tile_images(d.astype(np.uint8), size=display_size), 'RGB')
     img.save("/homes/jxu/projects/ImageInpainting/samples/complete.png")
