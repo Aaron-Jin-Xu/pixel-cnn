4a6c351a4764597049a5b0d2b34dcd5b06c2cb08
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-07 15:37:07 +0000

    fix

--- analyze.py
+++ analyze.py
@@ -1,11 +1,26 @@
 import numpy as np
 
-def load_record(dir):
+def load_records(dir):
     path = os.path.join(dir, "inpainting_record.npz")
-    return os.load(path)
+    d = os.load(path)
+    return d['img'], d['dis'], d['smp']
 
-def read_record(image_id):
-    pass
-
-def current_state(record, cur_image, next_dis):
-    pass
+def get_image_record(records, image_id, t="image", dist_type="combine"):
+    img, dis, smp = records
+    if t=='image':
+        return img[:, image_id, :, :, :]
+    elif t=='dist':
+        if dist_type=='forward':
+            return dis[:, :, 0, image_id, :]
+        elif dist_type=='backward':
+            return dis[:, :, 1, image_id, :]
+        elif dist_type=='combine':
+            return dis[:, :, 2, image_id, :]
+        elif dist_type=='prior':
+            return dis[:, :, 3, image_id, :]
+        else:
+            raise Exception(t+" type not found")
+    elif t=='sample':
+        return smp[:, image_id, :]
+    else:
+        raise Exception(t+" type not found")
