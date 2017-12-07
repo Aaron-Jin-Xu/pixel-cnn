3b24d6a7133c6426c2b9e96b3ffb5774434157f5
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-07 20:15:01 +0000

    fix

--- analyze.py
+++ analyze.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 def load_records(dir):
     path = os.path.join(dir, "inpainting_record.npz")
@@ -30,6 +31,8 @@ def get_image_record(records, image_id, t="image", dist_type="combine"):
         raise Exception(t+" type not found")
 
 def analyze_record(records, image_id):
+
+
     _, _, _, params = records
     num_images = params['num_images']
     assert image_id < num_images, "image_id too large"
@@ -46,11 +49,39 @@ def analyze_record(records, image_id):
         cur_combine_dis = combine[p]
         cur_sample = sample[p]
 
+        plot(cur_forward_dis, cur_backward_dis, cur_combine_dis, cur_image, cur_sample)
+        break
+
         print("red", cur_forward_dis[0])
         print("red", cur_backward_dis[0])
         print("red", cur_combine_dis[0])
         print("sample", cur_sample[0])
         print("------------------------")
+
+
+def plot(forward_dist, backward_dist, combine_dist, image, sample):
+    fig = plt.figure(figsize=(16,16))
+    ax = fig.add_subplot(2,2,1)
+    ax.imshow(image)
+    ax.axis("off")
+
+    # Red channel
+    ax = fig.add_subplot(2,2,2)
+    ax.plot(np.arange(256), forward_dist[0], label="forward")
+    ax.plot(np.arange(256), backward_dist[0], label="backward")
+    ax.plot(np.arange(256), combine_dist[0], label="combine")
+
+    ax.legend(loc=0)
+    ax.set_ylim(0., 0.15)
+    ax.set_title("Red Channel")
+    fig.savefig("analyze_report.png")
+
+
+
+
+
+
+
 
 
 records = load_records("/data/ziz/jxu")
