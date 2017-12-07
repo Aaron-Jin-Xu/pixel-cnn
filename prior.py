161278a016acd12e8f4819994489d779f23de985
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-07 23:43:28 +0000

    fix

--- prior.py
+++ prior.py
@@ -3,7 +3,7 @@ from PIL import Image
 from data.celeba_data import read_imgs
 
 #imgs = read_imgs("/data/ziz/not-backed-up/jxu/CelebA/celeba64-train")
-imgs= np.load("/data/ziz/not-backed-up/jxu/SVHN/SVHN32-train.npz")['arr']
+imgs= np.load("/data/ziz/not-backed-up/jxu/SVHN/SVHN32-train.npz")['X']
 
 num_samples = imgs.shape[0]
 p = np.array([(imgs==i).astype(np.uint32).astype(np.float32).sum(0) for i in range(256)])
