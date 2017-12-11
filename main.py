6ab6a28c2849273634acefe8ba0f38f1dca80db2
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-11 14:09:18 +0000

    fix

--- main.py
+++ main.py
@@ -66,8 +66,8 @@ with tf.Session() as sess:
     #mgen = mk.CircleMaskGenerator(obs_shape[0], obs_shape[1], 8)
     #mgen = mk.RectangleMaskGenerator(obs_shape[0], obs_shape[1])
     #mgen = mk.BottomMaskGenerator(obs_shape[0], obs_shape[1], 16)
-    #mgen = mk.HorizontalMaskGenerator(obs_shape[0], obs_shape[1], 8, 24)
-    mgen = mk.RandomNoiseMaskGenerator(obs_shape[0], obs_shape[1], 0.5)
+    mgen = mk.HorizontalMaskGenerator(obs_shape[0], obs_shape[1], 24, 40)
+    #mgen = mk.RandomNoiseMaskGenerator(obs_shape[0], obs_shape[1], 0.5)
     ms = mgen.gen(fm.args.nr_gpu * fm.args.batch_size)
     ms_ori = ms.copy()
 
