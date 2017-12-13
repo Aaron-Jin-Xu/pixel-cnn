71763bc22f1e480e143f37fbf7171fb07d9d9282
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-13 16:07:42 +0000

    fix

--- data/celeba_data_hr.py
+++ data/celeba_data_hr.py
@@ -15,13 +15,13 @@ def read_imgs(dir):
     return imgs
 
 def load(data_dir, subset='train'):
+    if subset=='valid':
+        trainx = read_imgs("/home/jxu/projects/semantic_image_inpainting/testimages")
+        trainy = np.ones((trainx.shape[0], ))
+        return trainx, trainy
     if subset in ['train', 'valid', 'test']:
         #trainx = np.load(os.path.join(data_dir, "img_cropped_celeba.npz"))['arr_0'][:200000, :, :, :]
         trainx = read_imgs(os.path.join(data_dir, "celeba64-{0}".format(subset)))
-        trainy = np.ones((trainx.shape[0], ))
-        return trainx, trainy
-    if subset=='valid':
-        trainx = read_imgs("/home/jxu/projects/semantic_image_inpainting/testimages")
         trainy = np.ones((trainx.shape[0], ))
         return trainx, trainy
     else:
