bd2f95f146612a93144bb39baae0d6aefbf4ad9e
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-14 09:35:03 +0000

    fix

--- evaluate.py
+++ evaluate.py
@@ -110,8 +110,8 @@ with tf.Session() as sess:
                 pars1 = params_to_dis(o1, fm.args.nr_logistic_mix)
                 pars2 = params_to_dis(o2, fm.args.nr_logistic_mix)
                 pars = pars1 * pars2 #/ pr[:, 0, :]
-                pars[:, 0] = 0.
-                pars[:, 255] = 0.
+                #pars[:, 0] = 0.
+                #pars[:, 255] = 0.
                 #pars = np.power(pars, 0.5)
                 pars = pars.astype(np.float64)
                 pars = pars / np.sum(pars, axis=-1)[:, None]
@@ -124,8 +124,8 @@ with tf.Session() as sess:
                 pars1 = params_to_dis(o1, fm.args.nr_logistic_mix, r=color_r)
                 pars2 = params_to_dis(o2, fm.args.nr_logistic_mix, r=color_r)
                 pars = pars1 * pars2 #/ pr[:, 1, :]
-                pars[:, 0] = 0.
-                pars[:, 255] = 0.
+                #pars[:, 0] = 0.
+                #pars[:, 255] = 0.
                 #pars = np.power(pars, 0.5)
                 pars = pars.astype(np.float64)
                 pars = pars / np.sum(pars, axis=-1)[:, None]
@@ -138,8 +138,8 @@ with tf.Session() as sess:
                 pars1 = params_to_dis(o1, fm.args.nr_logistic_mix, r=color_r, g=color_g)
                 pars2 = params_to_dis(o2, fm.args.nr_logistic_mix, r=color_r, g=color_g)
                 pars = pars1 * pars2 #/ pr[:, 2, :]
-                pars[:, 0] = 0.
-                pars[:, 255] = 0.
+                #pars[:, 0] = 0.
+                #pars[:, 255] = 0.
                 #pars = np.power(pars, 0.5)
                 pars = pars.astype(np.float64)
                 pars = pars / np.sum(pars, axis=-1)[:, None]
