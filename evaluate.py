c5617f19d5c166566211fd7be80cf9ed974fa2b1
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-14 12:59:48 +0000

    fix

--- evaluate.py
+++ evaluate.py
@@ -109,7 +109,7 @@ with tf.Session() as sess:
                 # Sample red channel
                 pars1 = params_to_dis(o1, fm.args.nr_logistic_mix)
                 pars2 = params_to_dis(o2, fm.args.nr_logistic_mix)
-                pars = pars1 * pars2 #/ pr[:, 0, :]
+                pars = pars1 * pars2 / pr[:, 0, :]
                 pars[:, 0], pars[:, 255] = pars[:, 1], pars[:, 254]
                 #pars[:, 0] = 0.
                 #pars[:, 255] = 0.
@@ -124,7 +124,7 @@ with tf.Session() as sess:
                 # Sample green channel
                 pars1 = params_to_dis(o1, fm.args.nr_logistic_mix, r=color_r)
                 pars2 = params_to_dis(o2, fm.args.nr_logistic_mix, r=color_r)
-                pars = pars1 * pars2 #/ pr[:, 1, :]
+                pars = pars1 * pars2 / pr[:, 1, :]
                 pars[:, 0], pars[:, 255] = pars[:, 1], pars[:, 254]
                 #pars[:, 0] = 0.
                 #pars[:, 255] = 0.
@@ -139,7 +139,7 @@ with tf.Session() as sess:
                 # Sample blue channel
                 pars1 = params_to_dis(o1, fm.args.nr_logistic_mix, r=color_r, g=color_g)
                 pars2 = params_to_dis(o2, fm.args.nr_logistic_mix, r=color_r, g=color_g)
-                pars = pars1 * pars2 #/ pr[:, 2, :]
+                pars = pars1 * pars2 / pr[:, 2, :]
                 pars[:, 0], pars[:, 255] = pars[:, 1], pars[:, 254]
                 #pars[:, 0] = 0.
                 #pars[:, 255] = 0.
