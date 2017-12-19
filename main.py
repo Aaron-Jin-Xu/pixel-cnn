b5ed746e6090e40128b940a2fd2aa96403af93a9
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-19 11:44:13 +0000

    fix

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def find_coutour(mask):
 #display_size = (6,6)
 display_size = (8,8)
 
-exp_label = "celeba-hr-half"
+exp_label = "celeba-hr-test"
 
 with tf.Session() as sess:
 
@@ -66,8 +66,8 @@ with tf.Session() as sess:
     #mgen = mk.RecNoProgressMaskGenerator(obs_shape[0], obs_shape[1])
     #mgen = mk.CircleMaskGenerator(obs_shape[0], obs_shape[1], 8)
     #mgen = mk.RectangleMaskGenerator(obs_shape[0], obs_shape[1])
-    #mgen = mk.BottomMaskGenerator(obs_shape[0], obs_shape[1], 16)
-    mgen = mk.HorizontalMaskGenerator(obs_shape[0], obs_shape[1], 8, 24)
+    mgen = mk.BottomMaskGenerator(obs_shape[0], obs_shape[1], 8)
+    #mgen = mk.HorizontalMaskGenerator(obs_shape[0], obs_shape[1], 8, 24)
     #mgen = mk.GridMaskGenerator(obs_shape[0], obs_shape[1], 8)
     #mgen = mk.RandomNoiseMaskGenerator(obs_shape[0], obs_shape[1], 0.8)
     ms = mgen.gen(fm.args.nr_gpu * fm.args.batch_size)
