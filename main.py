45f15dfbc961909454c855c96f6a59368351c32d
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-19 19:11:04 +0000

    fix

--- main.py
+++ main.py
@@ -64,10 +64,10 @@ with tf.Session() as sess:
     # generate masks
     obs_shape = d.shape[1:]
     #mgen = mk.RecNoProgressMaskGenerator(obs_shape[0], obs_shape[1])
-    mgen = mk.CircleMaskGenerator(obs_shape[0], obs_shape[1], 16)
+    #mgen = mk.CircleMaskGenerator(obs_shape[0], obs_shape[1], 16)
     #mgen = mk.RectangleMaskGenerator(obs_shape[0], obs_shape[1])
     #mgen = mk.BottomMaskGenerator(obs_shape[0], obs_shape[1], 16)
-    #mgen = mk.HorizontalMaskGenerator(obs_shape[0], obs_shape[1], 16, 48)
+    mgen = mk.HorizontalMaskGenerator(obs_shape[0], obs_shape[1], 16, 48)
     #mgen = mk.GridMaskGenerator(obs_shape[0], obs_shape[1], 8)
     #mgen = mk.RandomNoiseMaskGenerator(obs_shape[0], obs_shape[1], 0.8)
     ms = mgen.gen(fm.args.nr_gpu * fm.args.batch_size)
@@ -128,7 +128,7 @@ with tf.Session() as sess:
         pars1 = params_to_dis(o1, fm.args.nr_logistic_mix, log_scales_shift=2.)
         pars2 = params_to_dis(o2, fm.args.nr_logistic_mix)
         pars = pars1 * pars2 #/ pr[:, 0, :]
-        pars[:, 0], pars[:, 255] = pars[:, 1], pars[:, 254]
+        #pars[:, 0], pars[:, 255] = pars[:, 1], pars[:, 254]
         #pars = np.power(pars, 0.5)
         pars = pars.astype(np.float64)
         pars = pars / np.sum(pars, axis=-1)[:, None]
@@ -143,7 +143,7 @@ with tf.Session() as sess:
         pars1 = params_to_dis(o1, fm.args.nr_logistic_mix, r=color_r, log_scales_shift=2.)
         pars2 = params_to_dis(o2, fm.args.nr_logistic_mix, r=color_r)
         pars = pars1 * pars2 #/ pr[:, 1, :]
-        pars[:, 0], pars[:, 255] = pars[:, 1], pars[:, 254]
+        #pars[:, 0], pars[:, 255] = pars[:, 1], pars[:, 254]
         #pars = np.power(pars, 0.5)
         pars = pars.astype(np.float64)
         pars = pars / np.sum(pars, axis=-1)[:, None]
@@ -158,7 +158,7 @@ with tf.Session() as sess:
         pars1 = params_to_dis(o1, fm.args.nr_logistic_mix, r=color_r, g=color_g, log_scales_shift=2.)
         pars2 = params_to_dis(o2, fm.args.nr_logistic_mix, r=color_r, g=color_g)
         pars = pars1 * pars2 #/ pr[:, 2, :]
-        pars[:, 0], pars[:, 255] = pars[:, 1], pars[:, 254]
+        #pars[:, 0], pars[:, 255] = pars[:, 1], pars[:, 254]
         #pars = np.power(pars, 0.5)
         pars = pars.astype(np.float64)
         pars = pars / np.sum(pars, axis=-1)[:, None]
