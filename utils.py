2319006abc3b258da52d423f4ce424d6ecb046d9
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-19 16:52:22 +0000

    fix

--- utils.py
+++ utils.py
@@ -119,6 +119,11 @@ def params_to_dis(params, nr_mix, r=None, g=None, b=None):
             log_pdf_mid = mid_in - log_scales[:, 0, :] - 2. * softplus(mid_in)
             log_probs = np.where(x < -0.999, log_cdf_plus, np.where(x > 0.999, log_one_minus_cdf_min,
                                                             np.where(cdf_delta > 1e-5, np.log(np.maximum(cdf_delta, 1e-12)), log_pdf_mid - np.log(127.5))))
+            p = log_softmax(logit_probs)
+            p = np.exp(p)
+            p = p / np.sum(p, axis=-1)[:, None]
+            print(p)
+            print(p.shape)
             log_probs = log_probs + log_softmax(logit_probs)
             probs = sum_exp(log_probs)
             arr.append(probs)
