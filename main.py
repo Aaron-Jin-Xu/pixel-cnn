0d4b52dc9b8b617678c5c2eee03b5aed37adb70c
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-20 14:59:05 +0000

    fix

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ with tf.Session() as sess:
 
         # Sample red channel
         pars1 = params_to_dis(o1, fm.args.nr_logistic_mix, log_scales_shift=2.)
-        pars2 = params_to_dis(o2, fm.args.nr_logistic_mix*2)
+        pars2 = params_to_dis(o2, bm.args.nr_logistic_mix)
         pars = pars1 #* pars2 #/ pr[:, 0, :]
         pars[:, 0], pars[:, 255] = pars[:, 1], pars[:, 254]
         #pars = np.power(pars, 0.5)
@@ -141,7 +141,7 @@ with tf.Session() as sess:
 
         # Sample green channel
         pars1 = params_to_dis(o1, fm.args.nr_logistic_mix, r=color_r, log_scales_shift=2.)
-        pars2 = params_to_dis(o2, fm.args.nr_logistic_mix*2, r=color_r)
+        pars2 = params_to_dis(o2, bm.args.nr_logistic_mix, r=color_r)
         pars = pars1 #* pars2 #/ pr[:, 1, :]
         pars[:, 0], pars[:, 255] = pars[:, 1], pars[:, 254]
         #pars = np.power(pars, 0.5)
@@ -156,7 +156,7 @@ with tf.Session() as sess:
 
         # Sample blue channel
         pars1 = params_to_dis(o1, fm.args.nr_logistic_mix, r=color_r, g=color_g, log_scales_shift=2.)
-        pars2 = params_to_dis(o2, fm.args.nr_logistic_mix*2, r=color_r, g=color_g)
+        pars2 = params_to_dis(o2, bm.args.nr_logistic_mix, r=color_r, g=color_g)
         pars = pars1 #* pars2 #/ pr[:, 2, :]
         pars[:, 0], pars[:, 255] = pars[:, 1], pars[:, 254]
         #pars = np.power(pars, 0.5)
