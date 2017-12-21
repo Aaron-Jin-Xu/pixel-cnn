130bea71d0ac3c48a425bc7351a3ee42fbe10eec
Jin Xu <s1673820@sms.ed.ac.uk>
2017-12-21 19:45:48 +0000

    fix

--- configs.py
+++ configs.py
@@ -117,7 +117,7 @@ configs["celeba-hr-backward-rename"] = {
 
 configs["celeba-hr-test"] = {
     "data_dir": "/data/ziz/not-backed-up/jxu/CelebA",
-    "save_dir": "/data/ziz/jxu/save-test",
+    "save_dir": "/data/ziz/jxu/save64-forward-new-20",
     "nr_filters": 100,
     "nr_resnet": 4,
     "data_set": "celeba",
