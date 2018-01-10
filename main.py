4cc5da114796dc15e35571fdf1153781979f1bf3
Jin Xu <s1673820@sms.ed.ac.uk>
2018-01-10 23:49:20 +0800

    fix

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def find_coutour(mask):
 #display_size = (6,6)
 display_size = (8,8)
 
-exp_label = "svhn-nomap-middle"
+exp_label = "svhn-nomap-center"
 
 with tf.Session() as sess:
 
@@ -68,10 +68,10 @@ with tf.Session() as sess:
     #mgen = mk.CircleMaskGenerator(obs_shape[0], obs_shape[1], 16)
     #mgen = mk.RectangleMaskGenerator(obs_shape[0], obs_shape[1])
     #mgen = mk.BottomMaskGenerator(obs_shape[0], obs_shape[1], 32)
-    mgen = mk.HorizontalMaskGenerator(obs_shape[0], obs_shape[1], 10, 25)
+    #mgen = mk.HorizontalMaskGenerator(obs_shape[0], obs_shape[1], 10, 25)
     #mgen = mk.GridMaskGenerator(obs_shape[0], obs_shape[1], 8)
     #mgen = mk.RandomNoiseMaskGenerator(obs_shape[0], obs_shape[1], 0.8)
-    #mgen = mk.CenterMaskGenerator(obs_shape[0], obs_shape[1], 0.5)
+    mgen = mk.CenterMaskGenerator(obs_shape[0], obs_shape[1], 0.5)
     ms = mgen.gen(fm.args.nr_gpu * fm.args.batch_size)
     ms_ori = ms.copy()
 
