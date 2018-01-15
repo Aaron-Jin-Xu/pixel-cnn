9806dcb89201f763cd0cef70f1cd43188dce86e4
Jin Xu <s1673820@sms.ed.ac.uk>
2018-01-15 20:48:50 +0800

    fix

--- utils.py
+++ utils.py
@@ -125,7 +125,6 @@ def params_to_dis(params, nr_mix, r=None, g=None, b=None, MAP=False):
     log_scales = np.maximum(l[:, :, nr_mix:2 * nr_mix], -7.)
     print(means)
     print(log_scales)
-    quit()
     # log_scales -= log_scales_shift
     coeffs = np.tanh(l[:, :, 2 * nr_mix:3 * nr_mix])
     #if MAP:
