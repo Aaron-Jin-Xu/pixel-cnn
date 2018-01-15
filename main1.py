6c6cf0beba1ca444b88967f8d367a0af30a1aa0b
Jin Xu <s1673820@sms.ed.ac.uk>
2018-01-15 20:33:32 +0800

    fix

--- main1.py
+++ main1.py
@@ -75,7 +75,7 @@ with tf.Session() as sess:
     #mgen = mk.CenterMaskGenerator(obs_shape[0], obs_shape[1], 0.5)
     #mgen = mk.RightMaskGenerator(obs_shape[0], obs_shape[1], 0.5)
     #mgen = mk.RectangleMaskGenerator(obs_shape[0], obs_shape[1], 20, 61, 20, 32)
-    mgen = mk.RectangleMaskGenerator(obs_shape[0], obs_shape[1], 18, 23, 0, 64)
+    mgen = mk.RectangleMaskGenerator(obs_shape[0], obs_shape[1], 21, 23, 0, 64)
     ms = mgen.gen(fm.args.nr_gpu * fm.args.batch_size)
     ms_ori = ms.copy()
 
@@ -127,7 +127,7 @@ with tf.Session() as sess:
         # Forward model prediction
         #if flag=="forward":
             #feed_dict = fm.make_feed_dict(d, mask_values=ams, rot=False)
-        feed_dict = fm.make_feed_dict(d, mask_values=1-ams, rot=False)
+        feed_dict = fm.make_feed_dict(d, mask_values=feed_ms, rot=False)
         _o1 = sess.run(fm.outputs, feed_dict)
         _o1 = np.concatenate(_o1, axis=0)
         o1 = get_params(_o1, target_pixels)
