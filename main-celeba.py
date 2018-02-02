178c72a23e8c228b338c66d3a9efa7d96fd2b2c4
Jin Xu <s1673820@sms.ed.ac.uk>
2018-02-02 19:55:25 +0800

    fix

--- main-celeba.py
+++ main-celeba.py
@@ -60,8 +60,7 @@ with tf.Session() as sess:
     d = next(fm.test_data)
     #d = next(fm.test_data)
     #d = next(fm.test_data)
-    d = rgb_resize(d, 0.5)
-    d = rgb_resize(d, 2.0)
+    d[:, 16:48, 16:48, :] = rgb_resize(rgb_resize(d[:, 16:48, 16:48, :], 0.5), 2.0)
     # Store original images
     img = Image.fromarray(tile_images(d.astype(np.uint8), size=display_size), 'RGB')
     img.save("/homes/jxu/projects/ImageInpainting/plots1/original-{0}.png".format(exp_label))
