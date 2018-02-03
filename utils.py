2d8bce09b023977e4d089ae8f29338b3fd1ac998
Jin Xu <s1673820@sms.ed.ac.uk>
2018-02-03 22:32:29 +0800

    fix

--- utils.py
+++ utils.py
@@ -407,6 +407,9 @@ def combine_dis(coeffs1, dis_log_compons1, coeffs2, dis_log_compons2):
     coeffs1 /= np.sum(coeffs1, axis=-1)[:, None]
     shifted_dis_compons1 /= shifted_dis_compons1.sum(-1)[:,:, None]
 
+    print(coeffs1.sum(-1))
+    print(shifted_dis_compons1.sum(-1))
+    quit()
     coeffs1_stack = np.stack([coeffs1 for i in range(dis_log_compons1.shape[-1])], axis=-1)
     dis = np.sum(shifted_dis_compons1 * coeffs1_stack, axis=1)
 
